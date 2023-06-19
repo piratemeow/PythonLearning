c07918a8178b839a70435d58c7947b0c3d49ed05
piratemeow <imranrahman543@gmail.com>
2023-06-19 17:25:52 +0600

    lambda

---
+++ lambda.py
@@ -0,0 +1,11 @@
+# nameless functions with only one expression
+#These can not be invoked directly rather have to be assigned to a variabe first
+
+lambda num : num*2
+
+lamvar = lambda a,b : a*b
+lamvar2 = lambda s : print(s)
+
+
+print(lamvar(1,2))
+lamvar2('hello')
