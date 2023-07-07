3afd3d49b590d34e93bb3499614081236c054002
piratemeow <imranrahman543@gmail.com>
2023-07-07 14:25:03 +0600

    f

---
+++ annotation.py
@@ -0,0 +1,8 @@
+# Annotation simply tells the type of a variable, parameter of a function and return type
+# It is optional in python and the interpreter ignores it
+# It is for the programmer who will be using to fundtion or variable to use it as its correct type
+
+
+var: int = 0 # This variable is an integer
+def sum (a:int, b: int)->int:
+    return a+b
---
+++ docstring.py
@@ -0,0 +1,25 @@
+# Docstring
+# Is is used to document a function or a class in python
+# The help() function will print the contents of the docstring in a good formatted way
+
+"""This is used to add to numbers a & b"""
+
+def add(a,b):
+    """This is used to add to numbers a & b"""
+    return a+b
+    
+
+print(add(1,12))
+print(help(add))
+# Press Enter after every help() function call
+class doc:
+    def __init__ (self):
+        """This is the constructor"""
+
+    def pri():
+        """This is the pri function"""
+
+    a = input()
+    """This taked a input""" 
+    
+print(help(doc))
