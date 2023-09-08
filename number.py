6185c7110c67c30efab876f3270fbeb5487c3ee8
piratemeow <imranrahman543@gmail.com>
2023-09-09 04:57:32 +0600

    added some stuff in number.py

---
+++ number.py
@@ -0,0 +1,24 @@
+# abs(value) returns the absolute value
+# round(value) it rounds the value to the nearest integer
+print(round(12.3))
+
+b = 0b1010 # This is a binary number
+print(b) # This will print the decimal value of the binary number
+print(bin(b)) # This will print the binary value of the decimal number 
+d= int(b) # This will convert the binary number to decimal
+print(d)
+d = int('1010',3) # This will convert the binary number to decimal 
+print(d)
+
+d = int('123',4) # This will convert the number with base 4 to decimal
+print(d)
+
+h = hex(12) # This will convert the decimal number to hexadecimal
+print(h)
+
+o = 0o123 # This is a octal number
+print(o) # This will print the decimal value of the octal number
+print(oct(o)) # This will print the octal value of the decimal number   
+
+h = hex(o) # This will convert the octal number to hexadecimal  
+print(h)
--- numbers.py
+++
@@ -1,5 +0,0 @@
-# abs(value) returns the absolute value
-# round(value) it rounds the value to the nearest integer
-
-
-print(round(12.3))
