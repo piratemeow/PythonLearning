46866f55b62623b74e979c2264a8667a365ebdab
piratemeow <imranrahman543@gmail.com>
2023-09-09 04:44:21 +0600

    Added some extra stuff to tuples.py

--- tuples.py
+++ tuples.py
@@ -21,3 +21,19 @@ print(liss[0]) # This will return the first tuple
 print(liss[0][1]) # This will return the second element of the first tuple
 for i in enumerate(tup):
     print(i)
+
+tup2 = (1,2),(2,3),(3,4) # This is a tuple of tuples
+print(tup2)
+print(tup2[0])
+
+dic = dict(tup2) # This will convert the tuple of tuples to a dictionary
+print(dic[1]) # This will return the value of the key 1 as dictionary can not be accessed with index
+
+dic2 = {'a':1,'b':2,'c':3}
+tup3 = tuple(dic2.items()) # This will convert the dictionary to a tuple of tuples
+print(tup3)
+print(tup3[0]) # This will return the value of the key 0 as tuple of tuples can be accessed with index
+tup4 = tuple(dic2.values()) # This will convert the dictionary to a tuple of values 
+print(tup4)
+tup5 = tuple(dic2.keys()) # This will convert the dictionary to a tuple of keys tuple(dic2) will also do the same
+print(tup5)
