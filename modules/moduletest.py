056612a509530958f6a79f2cd47cbc1475d53914
piratemeow <imranrahman543@gmail.com>
2023-09-22 03:44:45 +0600

    added some more documentations

--- modules/moduletest.py
+++ modules/moduletest.py
@@ -6,3 +6,5 @@ dog.bark()
 ob = dog.ani
 ob.pri()
 
+print(dog.a) # prints the variable a from the dog file
+
--- modules/moduletest2.py
+++ modules/moduletest2.py
@@ -5,3 +5,5 @@ from dog import ani
 bark()
 
 ob = ani()
+
+# print(dog.a) will cause an error because a is not imported
